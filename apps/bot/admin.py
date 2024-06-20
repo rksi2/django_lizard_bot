3eb74d4fa48c6212171173c884756bbc5c15f9ea
Denis <claimov@gmail.com>
2024-06-20 12:54:03 +0300

    Add educators model and register it in admin panel

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,1 +1,6 @@
 """Модуль админки."""
+
+from django.contrib import admin
+from apps.bot.models import Educator
+
+admin.site.register(Educator)
--- apps/bot/models.py
+++ apps/bot/models.py
@@ -1,1 +1,12 @@
 """Модуль создания моделей БД."""
+
+from django.db import models
+
+
+class Educator(models.Model):
+    first_name = models.CharField(max_length=55, verbose_name="Имя")
+    last_name = models.CharField(max_length=55, verbose_name="Фамилия")
+    middle_name = models.CharField(max_length=55, verbose_name="Отчество")
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
